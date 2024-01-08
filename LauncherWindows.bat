165e35693c2572073777a67ca8f3a3078bc66b1b
Pablete <macos@MacBook-Air-de-MacOS.local>
2024-01-08 06:33:07 -0300

    fix windows launcher

--- LauncherWindows.bat
+++ LauncherWindows.bat
@@ -1,4 +1,13 @@
-@echo off
+@echo on
+
+:: Verificar permisos de administrador
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo Este script requiere privilegios de administrador.
+    echo Por favor, ejecútalo como administrador.
+    pause
+    exit /b 1
+)
 
 :: Comprobar si Python 3.10 está instalado
 where python >nul 2>&1
@@ -22,10 +31,20 @@ if exist %requirementsFile% (
     python -m pip install -r %requirementsFile%
 )
 
+:: Verificar existencia del archivo de configuración
+set configFile=config.txt
+if not exist %configFile% (
+    echo Por favor, configure el archivo %configFile% seleccionando su nombre de usuario.
+    pause
+    exit /b 1
+)
+
 :: Ejecutar Launch.py
 set launchScript=Launch.py
 if exist %launchScript% (
     python %launchScript%
 ) else (
     echo Error: No se encontró %launchScript%.
+    pause
+    exit /b 1
 )
